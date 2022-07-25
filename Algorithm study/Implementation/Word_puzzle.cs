265b9ea27d3fbea05b792ab9646fc11820aae335
Channy <61169601+Ikchan-ju@users.noreply.github.com>
2022-07-25 16:54:15 +0900

    Update Word_puzzle.cs

--- Algorithm study/Implementation/Word_puzzle.cs
+++ Algorithm study/Implementation/Word_puzzle.cs
@@ -3,25 +3,22 @@ using System.Linq;
 using System.Collections.Generic;
 
 public class Solution {
-    int answer = -1;
     Dictionary<string, bool> dict_strs = new Dictionary<string, bool>();
     public int solution(string[] strs,string t){
-        dict_strs = strs.ToDictionary(s => s, s => true);
-        Sol(t, 0, 1);
-        
-        return answer;
-    }
-    
-    public void Sol(string t, int start, int current){
-        for(int i = 1; i <= (5 > t.Length - start ? t.Length - start : 5); i++){
-            string str = t.Substring(start, i);
-            if(dict_strs.ContainsKey(str)){
-                if(start + str.Length == t.Length){
-                    if(answer == -1 || answer > current) answer = current;
-                    return;
+        int answer = -1;
+        int[] count_arr = t.ToCharArray().Select(c => t.Length + 1).ToArray();
+        for(int i = 0; i < t.Length; i++){
+            for(int j = 0; j < strs.Length; j++){
+                if(t.Substring(i, strs[j].Length).Equals(strs[j])){
+                    if(i < strs[j].Length){
+                        count_arr[i] = 1;
+                    }else{
+                        count_arr[i] = (count_arr[i] > 1 + count_arr[i - strs[j].Length] ? 1 + count_arr[i - strs[j].Length] : count_arr[i]);
+                    }
                 }
-                Sol(t, start + str.Length, current + 1);
             }
         }
+        answer = (count_arr.Max() > t.Length ? answer : count_arr.Max());
+        return answer;
     }
  }
