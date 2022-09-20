6bb0a7ba30abee558e2bb159c0b678e3be0fef79
Channy <61169601+Ikchan-ju@users.noreply.github.com>
2022-09-20 14:38:36 +0900

    09/20 update

--- Algorithm study/Simplify/Python_one_line_coding.py
+++ Algorithm study/Simplify/Python_one_line_coding.py
@@ -35,3 +35,128 @@ def solution(x):
     return x % sum(map(int, str(x))) == 0
 
 solution = lambda x : int(x)
+
+
+# 이상한 문자 만들기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12930
+
+def solution(s):
+    return ' '.join(''.join([val.upper() if i % 2 == 0 else val.lower() for i, val in enumerate(word)]) for word in s.split(' '))
+
+
+# 시저 암호
+# https://school.programmers.co.kr/learn/courses/30/lessons/12926
+
+def solution(s, n):
+    return ' '.join(''.join(chr((ord(c) + n - 65) % 26 + 65) if 65 <= ord(c) < 91  else chr((ord(c) + n - 97) % 26 + 97) for c in w) for w in s.split(' '))
+
+
+# [1차] 비밀지도
+# https://school.programmers.co.kr/learn/courses/30/lessons/17681
+
+def solution(n, arr1, arr2):
+    return [''.join('#' if int(a) else ' ' for a in bin(arr1[i]|arr2[i])[2:].rjust(n, '0')) for i in range(n)]
+
+
+# 문자열 내 마음대로 정렬하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12915
+
+def solution(strings, n):
+    return sorted(sorted(strings), key = lambda x : x[n])
+
+
+# 서울에서 김서방 찾기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12919
+
+def solution(seoul):
+    return f'김서방은 {seoul.index("Kim")}에 있다'
+
+
+# 핸드폰번호 가리기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12948
+
+def solution(phone_number):
+    return '*' * (len(phone_number) - 4) + phone_number[-4:]
+
+
+# 나누어 떨어지는 숫자배열
+# https://school.programmers.co.kr/learn/courses/30/lessons/12910
+
+def solution(arr, divisor):
+    return [num for num in sorted(arr) if num % divisor == 0] or [-1]
+
+
+# 제일 작은 수 제거하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12935
+
+def solution(arr):
+    return arr[:arr.index(min(arr))] + arr[arr.index(min(arr)) + 1:] or [-1]
+
+# 수박수박수박수박수박수?
+# https://school.programmers.co.kr/learn/courses/30/lessons/12922
+
+def solution(n):
+    return '수박' * (n // 2) + '수' * (n % 2)
+
+
+# 가운데 글자 가져오기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12903
+
+def solution(s):
+    return s[:len(s)//2 + 1][-((len(s) + 1) % 2 + 1):]
+
+
+# 문자열 내림차순으로 배치하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12917
+
+def solution(s):
+    return ''.join(sorted(s, reverse = True))
+
+
+# 문자열 다루기 기본
+# https://school.programmers.co.kr/learn/courses/30/lessons/12918
+
+def solution(s):
+    return (len(s) == 4 or len(s) == 6) and s.isdigit()
+
+
+# 행렬의 덧셈
+# https://school.programmers.co.kr/learn/courses/30/lessons/12950
+
+def solution(arr1, arr2):
+    return list(map(lambda A, B : [a + b for a, b in zip(A, B)], arr1, arr2))
+
+
+# 직사각형 별찍기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12969
+
+a, b = map(int, input().strip().split(' '))
+print("\n".join(['*' * a] * b))
+
+
+# JadenCase 문자열 만들기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=python3#
+
+def solution(s):
+    return ' '.join((word[0].upper() + word[1:].lower() if len(word) > 0 else '') for word in s.split(' '))
+
+
+# 최솟값 만들기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12941
+
+def solution(A,B):
+    return sum(map(lambda a, b: a * b, sorted(A), sorted(B, reverse = True)))
+
+
+# x만큼 간격이 있는 n개의 숫자
+# https://school.programmers.co.kr/learn/courses/30/lessons/12954
+
+def solution(x, n):
+    return list(range(x, x * (n + 1), x)) if x != 0 else [0] * n
+
+
+# 두 정수사이의 합
+# https://school.programmers.co.kr/learn/courses/30/lessons/12912?language=python3
+
+def solution(a, b):
+    return sum(range(a, b + 1)) if a < b else sum(range(b, a + 1))
