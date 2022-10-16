098916631a297609bfbe4acc42ba9d92ff3324f1
ikchan-ju <couquedasom@gmail.com>
2022-10-16 23:25:55 +0900

    10/16 update

--- Algorithm study/Two_pointer/Jewel_shopping.py
+++ Algorithm study/Two_pointer/Jewel_shopping.py
@@ -4,12 +4,17 @@
 import collections
 def solution(gems):
     dic = {val : idx for idx, val in enumerate(collections.Counter(gems))}
+    keys = list(dic.keys())
     answer, counts, start = [1, len(gems)], [0 for _ in range(len(dic))], 0
-    for i in range(len(gems)):
-        counts[dic[gems[i]]] += 1
-        while counts.count(0) == 0:
+    for i, g in enumerate(gems):
+        if g in keys:
+            keys.remove(g)
+        counts[dic[g]] += 1
+        while len(keys) == 0:
             if answer[1] - answer[0] > i - start:
                 answer = [start + 1, i + 1]
             counts[dic[gems[start]]] -= 1
+            if counts[dic[gems[start]]] == 0:
+                keys.append(gems[start])
             start += 1
     return answer
