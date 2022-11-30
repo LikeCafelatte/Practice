899b37cd596e82bfeab46eb3fc5de8e98c44525b
ikchan-ju <couquedasom@gmail.com>
2022-11-30 22:51:33 +0900

    11/30 update

--- Algorithm study/Exhaustive_Search/Knights_weapons.py
+++ Algorithm study/Exhaustive_Search/Knights_weapons.py
@@ -2,14 +2,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/136798
 
 def solution(number, limit, power):
-    answer = 0
+    weapons = [0 for _ in range(number + 1)]
+    limited = [False for _ in range(number + 1)]
     for i in range(1, number + 1):
-        weapon = 0
-        for j in range(1, i + 1):
-            if i % j == 0:
-                weapon += 1
-                if weapon > limit:
-                    weapon = power
-                    break
-        answer += weapon
-    return answer
+        for j in range(1, number // i + 1):
+            if not limited[i * j]:
+                weapons[i * j] += 1
+                if weapons[i * j] > limit:
+                    weapons[i * j] = power
+                    limited[i * j] = True
+    return sum(weapons)
